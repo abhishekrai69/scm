f926e61acb20ef46c3bbd4c0cf8789d5accdd8f0
Abhishek Kumar Rai <122802561+abhishekrai69@users.noreply.github.com>
2023-02-09 15:24:25 +0530

    c programing

---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("write anything on this paragraph");
+    return 0;
+}
---
+++ min.c
@@ -0,0 +1,10 @@
+//WAP TO ADD SUM OF THREE NUMBERS
+#include <stdio.h>
+int main()
+{
+    int a,b,sum;
+    printf("enter three number:\n");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum=%d\n",sum);
+}
